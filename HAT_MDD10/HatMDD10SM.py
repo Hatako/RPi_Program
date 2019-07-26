2d46e55dd5d6662af227cf872bf54d0704f4e276
hatako <hakureishrine00@gmail.com>
2019-07-26 23:15:43 +0900

    MDD10no

--- HAT_MDD10/HatMDD10SM.py
+++ HAT_MDD10/HatMDD10SM.py
@@ -10,46 +10,45 @@ GPIO.setup(AN2, GPIO.OUT)		# set pin as output
 GPIO.setup(AN1, GPIO.OUT)		# set pin as output
 GPIO.setup(DIG2, GPIO.OUT)		# set pin as output
 GPIO.setup(DIG1, GPIO.OUT)		# set pin as output
-sleep(1)				# delay for 1 seconds
+print("please wait")
+sleep(1)				# delay for 5 seconds
+print("go")
 p1 = GPIO.PWM(AN1, 100)			# set pwm for M1
 p2 = GPIO.PWM(AN2, 100)			# set pwm for M2
+
+p1.start(0)
+p2.start(0)
 
 try:					
   while True:
 
-   print "Left"				# display "Forward" when programe run
+   print ("左")				# display "Forward" when programe run
    GPIO.output(DIG1, GPIO.HIGH)		# set DIG1 as HIGH, M1B will turn ON
    GPIO.output(DIG2, GPIO.LOW)		# set DIG2 as HIGH, M2B will turn ON
-   p1.start(100)			# set speed for M1 at 100%
-   p2.start(100)			# set speed for M2 at 100%
-   sleep(2)				#delay for 2 second
-   print "Forward"
+   p1.start(20)			# set speed for M1 at 100%
+   p2.start(20)			# set speed for M2 at 100%
+   sleep(5)				#delay for 2 second
+
+   print ("後退")
    GPIO.output(DIG1, GPIO.LOW)          # set DIG1 as LOW, to control direction
    GPIO.output(DIG2, GPIO.LOW)          # set DIG2 as LOW, to control direction
-   p1.start(100)                        # set speed for M1 at 100%
-   p2.start(100)                        # set speed for M2 at 100%
-   sleep(2)                             #delay for 2 second
+   p1.start(20)                        # set speed for M1 at 100%
+   p2.start(20)                        # set speed for M2 at 100%
+   sleep(5)                             #delay for 2 second
 
-   print "Backward"
+   print("前進")
    GPIO.output(DIG1, GPIO.HIGH)         # set DIG1 as HIGH, to control direction
    GPIO.output(DIG2, GPIO.HIGH)         # set DIG2 as HIGH, to control direction
-   p1.start(100)                        # set speed for M1 at 100%
-   p2.start(100)                        # set speed for M2 at 100%
-   sleep(2)                             #delay for 2 second
+   p1.start(20)                        # set speed for M1 at 100%
+   p2.start(20)                        # set speed for M2 at 100%
+   sleep(5)                             #delay for 2 second
 
-   print "Right"
-   GPIO.output(DIG1, GPIO.LOW)       
-   GPIO.output(DIG2, GPIO.HIGH)    
-   p1.start(100)                     
-   p2.start(100)                   
-   sleep(2)                        
-
-   print "STOP"
-   GPIO.output(DIG1, GPIO.LOW)          # Direction can ignore
-   GPIO.output(DIG2, GPIO.LOW)          # Direction can ignore
-   p1.start(0)                          # set speed for M1 at 0%
-   p2.start(0)                          # set speed for M2 at 0%
-   sleep(3)                             #delay for 3 second
+   print ("右")
+   GPIO.output(DIG1, GPIO.LOW)
+   GPIO.output(DIG2, GPIO.HIGH)
+   p1.start(20)
+   p2.start(20)
+   sleep(5)
 
 
 except:					# exit programe when keyboard interupt
